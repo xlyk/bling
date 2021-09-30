422262573c189e01eb83c28061f38c6673d426b0
Kyle Hanks <kylehanks@pm.me>
2021-09-30 10:57:37 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/xlyk/bling
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package bling
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/nu7hatch/gouuid"
+	"strconv"
+)
+
+type Context struct {
+	UniqueID  string
+	RequestId int64
+}
+
+var (
+	headerUniqueID  = "bling-uid"
+	headerRequestID = "bling-rid"
+)
+
+func init() {
+	fmt.Println("[BLING] bling!")
+}
+
+func Bling(c *gin.Context) {
+	blingCtx, existing := detectContext(c)
+	if existing {
+		fmt.Printf("[BLING] found existing bling context (uid: %s, rid: %d)\n", blingCtx.UniqueID, blingCtx.RequestId)
+		blingCtx.RequestId += 1
+	} else {
+		fmt.Printf("[BLING] created new bling context (uid: %s)\n", blingCtx.UniqueID)
+	}
+
+	updateHeaders(c, blingCtx)
+
+	c.Next()
+}
+
+func newContext() *Context {
+	uid, _ := uuid.NewV4()
+	return &Context{UniqueID: uid.String(), RequestId: 1}
+}
+
+func detectContext(c *gin.Context) (Context, bool) {
+	uid := c.GetHeader(headerUniqueID)
+	if uid == "" {
+		return *newContext(), false
+	}
+
+	requestId := c.GetHeader(headerRequestID)
+	if requestId == "" {
+		requestId = "0"
+	}
+
+	rid, err := strconv.ParseInt(requestId, 10, 64)
+	if err != nil {
+		rid = 0
+	}
+
+	b := Context{
+		UniqueID:  uid,
+		RequestId: rid,
+	}
+
+	return b, true
+}
+
+func updateHeaders(c *gin.Context, b Context) {
+	c.Set(headerUniqueID, b.UniqueID)
+	c.Set(headerRequestID, b.RequestId)
+}
